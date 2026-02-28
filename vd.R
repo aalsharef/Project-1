710b543e1353c8d9472fef23855f54e6cb56c298
Abdullah Alsharef <aalsharef@ksu.edu.sa>
2026-02-28 18:39:57 +0300

    edit 2

---
+++ vd.R
@@ -0,0 +1,4 @@
+git add .                    # stage all changes
+git commit -m "your message" # save a snapshot
+git status                   # see what's changed
+git log                      # see commit history
